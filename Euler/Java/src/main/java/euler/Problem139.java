3b3fc450aa93c81cf2d75ddb887e9331433192c7
daniel <daniel.yuan@me.com>
2018-04-13 07:33:42 +0800

    solved euler problem #122,131,139

---
+++ Euler/Java/src/main/java/euler/Problem139.java
@@ -0,0 +1,37 @@
+package euler;
+
+import static euler.util.Utils.gcd;
+import static java.lang.Math.min;
+import static java.lang.Math.sqrt;
+
+public class Problem139 {
+    private final int limit;
+
+    public Problem139(int limit) {
+        this.limit = limit;
+    }
+
+    public int solve() {
+        int count = 0;
+        int max_m = (int)sqrt(limit / 2);
+        for (int m = 2; m <= max_m; m++) {
+            int max_n = min(m - 1, limit / (2 * m) - m);
+            for (int n = (m & 1) + 1; n <= max_n; n += 2) {
+                if (gcd(m, n) == 1) {
+                    int a = m * m - n * n;
+                    int b = 2 * m * n;
+                    int c = m * m + n * n;
+                    if (c % (b - a) == 0) {
+                        count += limit / (a + b + c);
+                    }
+                }
+            }
+        }
+        return count;
+    }
+
+    public static void main(String[] args) {
+        Problem139 solver = new Problem139(100_000_000);
+        System.out.println(solver.solve());
+    }
+}
