ec6baec0bed36f49895571e92e2b4ec83808b5a8
daniel <daniel.yuan@me.com>
2018-05-02 17:09:38 +0800

    solved euler problem #288

---
+++ Euler/Java/src/main/java/euler/Problem288.java
@@ -0,0 +1,28 @@
+package euler;
+
+import static euler.util.Utils.pow;
+
+public final class Problem288 {
+    private Problem288() {}
+
+    public static long solve(long prime, int limit, int exponent) {
+        long m = pow(prime, exponent);
+        long s = 290797;
+        long x = 0;
+        long r = 0;
+
+        for (int n = 0; n <= limit; n++) {
+            long t = s % prime;
+            r = (r + t * x) % m;
+            if (n < exponent)
+                x = x * prime + 1;
+            s = s * s % 50515093;
+        }
+        return r;
+    }
+
+    public static void main(String[] args) {
+        System.out.println(solve(3, 10000, 20));
+        System.out.println(solve(61, 10_000_000, 10));
+    }
+}
